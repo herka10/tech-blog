59b77a572cae4739ac1aa475cbc01944133902d4
herka10 <kaonou.her10@gmail.com>
2023-01-05 22:19:08 -0600

    add new post works

--- public/js/blog.js
+++ public/js/blog.js
@@ -4,34 +4,32 @@ const handleSubmit = event => {
     event.preventDefault()
     
     console.log("hi, submit worked!")
-    console.log(event.target)
-    //const formId = event.target.id
-    // const url =  '/api/blogs'
+    
+    //const formId = event.target
+    const url =  '/api/blogs'
 
     
 
-    //const { title, description } = event.target.elements
+    const { title, description } = event.target.elements
 
-    // const userData = {
-    //     title: title.value, 
-    //     description: description.value
-    // }
+    const userData = {
+        title: title.value, 
+        description: description.value
+    }
 
-    //console.log('data:', userData)
-
-    // fetch (url, {
-    //     method: 'POST',
-    //     headers: {
-    //         'Content-TYPE': 'application/json'
-    //     },
-    //     body: JSON.stringify(userData)
-    // })
-    //     .then(response => {
-    //         if (response.status === 200) {
-    //             window.location.href = '/'
-    //         }
-    //     })
-    //     .catch(err => console.log(err))
+    fetch (url, {
+        method: 'POST',
+        headers: {
+            'Content-TYPE': 'application/json'
+        },
+        body: JSON.stringify(userData)
+    })
+        .then(response => {
+            if (response.status === 200) {
+                window.location.href = '/'
+            }
+        })
+        .catch(err => console.log(err))
 }
 
 submitPost.addEventListener('submit', handleSubmit)
